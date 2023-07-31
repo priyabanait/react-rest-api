d412bf9b69cba57512a7f3b0a9f650c9cc7cf5aa
priyabanait <priyabanait91@gmail.com>
2023-07-31 23:47:53 +0530

    Making Api calls in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import MoviesList from './components/MoviesList';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const[movies,setMovie]=useState([]);
+  
+ function fetchmovies(){
+  fetch('https://swapi.dev/api/films/').then((response)=>{
+ return response.json();
+  }).then((data)=>{
+    const transferData = data.results.map(moviedata=>{
+      return {
+        id:moviedata.episode_id,
+        title:moviedata.title,
+        openingText:moviedata.opening_crawl,
+        releaseDate:moviedata.release_date
+
+      }
+    })
+    setMovie (transferData);
+  })
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchmovies}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
